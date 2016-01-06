262e23e3d87e7b2441da5534d3b38ac95285ec8f
rafeh01 <rafehqazi1@gmail.com>
2016-01-06 07:27:48 -0600

    The turn by turn instructions are fully functional.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 /**
  *
- * Created by ChessTastic on 1/6/16.
+ * Created by Rafeh Qazi on 1/6/16.
  */
+
+// A Google Maps Directions API request takes the following form:
+// https://maps.googleapis.com/maps/api/directions/output?parameters
+
+// Example:
+// The following request returns driving directions from Toronto, Ontario to Montreal, Quebec:
+// https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=YOUR_API_KEY
+
+function strip(html)
+{
+    var tmp = document.createElement("DIV");
+    tmp.innerHTML = html;
+    return tmp.textContent || tmp.innerText || "";
+}
 
 $(document).ready(function() {
     var API_KEY = "AIzaSyC4NDN-0uaL7Jn44lEz5Bd4fJGQ69pHcGA";
@@ -10,31 +24,18 @@ $(document).ready(function() {
     var URL = "https://maps.googleapis.com/maps/api/directions/json?origin="
         + loc1 + "&destination=" + loc2 + "&key=" + API_KEY;
 
-    console.log(loc1);
-    console.log(URL);
 
     $.getJSON(URL, function(json) {
-        console.log("hi");
-        console.log(json);
+        var steps = json["routes"][0]["legs"][0]["steps"];
+        for (i in steps) {
+            console.log(strip(steps[i]["html_instructions"]));
+        }
     });
 
 
-});
 
-/*
-$.getScript("../secret_api.js", function(){
 
-    alert("Script loaded but not necessarily executed.");
 
 });
-*/
 
 
-// A Google Maps Directions API request takes the following form:
-    // https://maps.googleapis.com/maps/api/directions/output?parameters
-
-    // Example:
-    // The following request returns driving directions from Toronto, Ontario to Montreal, Quebec:
-// https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=YOUR_API_KEY
-
-    // Locations
